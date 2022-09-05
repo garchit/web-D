ebefa718026dc5aadddabafd4e746441ae07eccd
Archit <garchit84@gmail.com>
2022-09-05 19:53:49 -0400

    funcstions

--- intermediate/functions_2.js
+++ intermediate/functions_2.js
@@ -1,3 +1,21 @@
 function getUserRole(name , role){
-    switch
+    switch (role){
+        case "admin":
+            return `${name} is admin with all access`
+            break;
+        case "subadmin":
+            return `${name} is sub-admin with access`
+            break;
+        case "subadmin":
+            return `${name} is sub-admin with access`
+            break;
+        case "tester":
+            return `${name} is tester with access`
+            break;
+        default :
+            return `${name} is trial user `
+            break;
+    }
 }
+// here break statement is not necessary .. 
+console.log(getUserRole("Hitesh" , "tester"));
